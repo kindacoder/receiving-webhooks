8a4a92db524b9ff4a66a42cd784b8668052fec1b
kindacoder <code.ashutosh@gmail.com>
2018-03-28 21:42:15 +0530

    Setting webhooks with smee.io

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express=require('express');
 const app=express();
 
 
+const SmeeClient = require('smee-client')
+
+const smee = new SmeeClient({
+  source: 'https://smee.io/6E9yNBRaFdiKO80',
+  target: 'http://localhost:3000/webhooks',
+  logger: console
+})
+
+const events = smee.start()
+
+// Stop forwarding events
+events.close()
+
 app.post('/webhooks',(req,res)=>{
-  
+
 
 })
 
